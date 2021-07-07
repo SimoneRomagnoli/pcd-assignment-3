7629a7a745fe96c9d5154432aac3becef7c6a77a
SimoneRomagnoli <simone.romagnoli10@studio.unibo.it>
2021-07-07 17:27:42 +0200

    change main name

--- src/main/scala/part2/akka/DistributedPuzzle.scala
+++ src/main/scala/part2/akka/DistributedPuzzle.scala
@@ -1,6 +1,6 @@
 package part2.akka
 
-object Application extends App {
+object DistributedPuzzle extends App {
   val imgPath: String = "res/bletchley-park-mansion.jpg"
   val puzzleBoard = PuzzleBoard(3, 5, imgPath)
 }
