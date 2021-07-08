855dcf7e84bc4bbef97762437586c3e872a498fb
SimoneRomagnoli <simone.romagnoli10@studio.unibo.it>
2021-07-08 12:50:02 +0200

    ask to join to players

--- src/main/scala/part2/akka/PuzzleBehaviors.scala
+++ src/main/scala/part2/akka/PuzzleBehaviors.scala
@@ -1,19 +1,18 @@
 package part2.akka
 
-import akka.actor.Status.{Failure, Success}
 import akka.actor.typed.{ActorRef, Behavior}
 import akka.actor.typed.receptionist.{Receptionist, ServiceKey}
-import akka.actor.typed.scaladsl.AskPattern.Askable
 import akka.actor.typed.scaladsl.Behaviors
 import akka.cluster.typed.{Cluster, Join}
-import part2.akka.PuzzleBehaviors.Messages.{Event, JoinRequest, JoinResponse}
+import akka.remote.transport.ActorTransportAdapter.AskTimeout
+import part2.akka.PuzzleBehaviors.Messages.Event
+
+import scala.util.{Failure, Success}
 
 object PuzzleBehaviors {
 
   object Messages {
     sealed trait Event
-    final case class JoinRequest(replyTo:ActorRef[Event]) extends Event with CborSerializable
-    final case class JoinResponse() extends CborSerializable
   }
 
   object Starter {
@@ -32,19 +31,23 @@ object PuzzleBehaviors {
   }
 
   object Joiner {
+    private case class ListingResponse(listing: Receptionist.Listing) extends Event
+    private final case class TransformCompleted(originalText: String, transformedText: String) extends Event
+    private final case class JobFailed(why: String, text: String) extends Event
+
     def apply(): Behavior[Event] = Behaviors.setup[Event] { ctx =>
       val cluster = Cluster(ctx.system)
+      val listingResponseAdapter = ctx.messageAdapter[Receptionist.Listing](ListingResponse)
+      ctx.system.receptionist ! Receptionist.Find(Player.PlayerServiceKey, listingResponseAdapter)
 
-      /*
-      ctx.ask(ctx.system.receptionist.narrow, JoinRequest) {
-        case Success(_) => ???
-        case Failure(_) => ???
+      Behaviors.receiveMessage {
+        case ListingResponse(Player.PlayerServiceKey.Listing(listings)) =>
+          ctx.ask(listings.head, Player.TransformText("bla", _)) {
+            case Success(transformedText) => TransformCompleted("BLA", transformedText.text)
+            case Failure(ex) => JobFailed("Processing timed out", "bla")
+          }
+          Behaviors.same
       }
-
-       */
-
-      cluster.manager ? JoinRequest(ctx.self)
-
       cluster.manager ! Join(cluster.selfMember.address)
 
       Behaviors.empty
@@ -52,14 +55,21 @@ object PuzzleBehaviors {
   }
 
   object Player {
-    val playerServiceKey: ServiceKey[Event] = ServiceKey[Event]("Player")
+    val PlayerServiceKey: ServiceKey[Command] = ServiceKey[Command]("Player")
 
-    def apply(id:Int, puzzleBoard: PuzzleBoard): Behavior[Event] = Behaviors.setup { ctx =>
+    sealed trait Command
+    final case class TransformText(text: String, replyTo: ActorRef[TextTransformed]) extends Command with CborSerializable
+    final case class TextTransformed(text: String) extends CborSerializable
+
+    //final case class JoinRequest(replyTo:ActorRef[Event]) extends Event with CborSerializable
+    //final case class JoinResponse() extends CborSerializable
+
+    def apply(id:Int, puzzleBoard: PuzzleBoard): Behavior[Command] = Behaviors.setup { ctx =>
       ctx.log.info(s"Registering myself (${ctx.self.path}) with receptionist")
-      ctx.system.receptionist ! Receptionist.Register(playerServiceKey, ctx.self)
+      ctx.system.receptionist ! Receptionist.Register(PlayerServiceKey, ctx.self)
 
       Behaviors.receiveMessage {
-        case JoinRequest(replyTo) =>
+        case TransformText(text, replyTo) =>
           ctx.log.info("i received a join request")
           val maxMembers = ctx.system.settings.config.getInt("distributed_puzzle_info.max-members")
           if(Cluster(ctx.system).state.members.size < maxMembers) {
